94b7ae953226ba428edcbf7823ddb3151d2822ae
OntologicalShock <justineldridgedavis@gmail.com>
2025-01-14 10:43:08 -0500

    Removing some pictures and adding new ones with Exif attributions embedded. Work to continue to retroactively update existing pictures as time allows.

---
+++ Wallpapers/exif-nord.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+exiftool -overwrite_original -Author="Peter Lam CH https://unsplash.com/@peterlamch" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/inside-orange-abandoned-building-with-different-things-inside-6TMW3HwrzuM" nord-pripyat-classroom-peter-lam-ch-unsplash.*\
+
+exiftool -overwrite_original -Author="Peter Lam CH https://unsplash.com/@peterlamch" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/white-concrete-wall-AifbysAZ66k" nord-broken-relays-peter-lam-ch-unsplash.*\
+
+exiftool -overwrite_original -Author="Peter Lam CH https://unsplash.com/@peterlamch" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/boxes-and-table-inside-abandoned-building-oDCmS-72P1U" nord-abandoned-peter-lam-unsplash.*\
+
+exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="Pripyat city - Chernobyl's Exclusion Zone, Ukraine October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/brown-wooden-bench-near-green-trees-during-daytime-77qt7n_bWO0" nord-pripyat-pool-viktor-hesse-unsplash.*\
+
+exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/white-concrete-house-surrounded-by-trees-during-daytime-BPtghEPUN7k" nord-abandoned-viktor-hesse-unsplash.*\
+
+exiftool -overwrite_original -Author="Michael Getreu https://unsplash.com/@talookon" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/a-small-pond-in-a-grassy-area-RZHcTjYNz4Y" nord-cnpp-michael-getreu-unsplash.*\
+
+exiftool -overwrite_original -Author="Mads Eneqvist https://unsplash.com/@madseneqvist" -Comment="Chernobyl - Pripyat, Ukraine - April 2009 Bridge of Death" -Description="https://unsplash.com/photos/a-dirt-road-with-benches-on-the-side-of-it-_PLtA43x3Bg" nord-bridge-of-death-mads-eneqvist-unsplash.*\
+
+exiftool -overwrite_original -Author="Mads Eneqvist https://unsplash.com/@madseneqvist" -Comment="Chernobyl - Pripyat, Ukraine - April 2009 Reactor 4" -Description="https://unsplash.com/photos/a-view-of-a-city-from-a-distance-Hwm1B4ZfNvU" nord-pripyat-mads-eneqvist-unsplash.*\
+
+exiftool -overwrite_original -Author="Mads Eneqvist https://unsplash.com/@madseneqvist" -Comment="Chernobyl - Pripyat, Ukraine - April 2009" -Description="https://unsplash.com/photos/a-factory-with-a-lot-of-smoke-coming-out-of-it-PpKvtSEiUCE" nord-cnpp-mads-eneqvist-unsplash.*\
+
+exiftool -overwrite_original -Author="Mads Eneqvist https://unsplash.com/@madseneqvist" -Comment="Chernobyl - Pripyat, Ukraine - April 2009" -Description="https://unsplash.com/photos/a-train-track-with-a-sky-in-the-background-us6J50X1f5I" nord-rails-mads-eneqvist-unsplash.*\
+
+exiftool -overwrite_original -Author="Mads Eneqvist https://unsplash.com/@madseneqvist" -Comment="Chernobyl - Pripyat, Ukraine - April 2009" -Description="https://unsplash.com/photos/a-train-track-running-through-a-city-with-tall-buildings-in-the-background-BSzqeneXSWA" nord-palace-of-culture-mads-eneqvist-unsplash.*\
+
+exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/brown-wooden-building-during-daytime-a4EkVA7efOw" nord-warehouse-kato-blackmore-unsplash*
