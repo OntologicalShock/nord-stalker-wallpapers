f143d305ffb084ba4c96dbd0e6e56721eb6caf23
OntologicalShock <justineldridgedavis@gmail.com>
2025-01-14 15:49:42 -0500

    Adding more pictures and EXIf attribution data to them.

--- Wallpapers/exif-nord.sh
+++ Wallpapers/exif-nord.sh
@@ -1,57 +1,29 @@
 #!/bin/bash
-exiftool -overwrite_original -Author="Gerhard Reus https://unsplash.com/@gerhardreus" -Comment="https://unsplash.com/photos/brown-wooden-house-near-road-during-daytime-o9QiVvTgUf0" -Description="Chernobyl, Kyiv Oblast, Ukraine" nord-bus-stop-gerhard-reus-unsplash.*\
+exiftool -overwrite_original -Author="Annie Spratt https://unsplash.com/@anniespratt" -Comment="N/A" -Description="https://unsplash.com/photos/a-close-up-of-a-plant-with-yellow-flowers-FOvTfxADPQQ" nord-frosty-yellow-flowers-annie-spratt-unsplash.*\
 
-exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="https://unsplash.com/photos/brown-wooden-house-surrounded-by-trees-during-daytime-JrLGvaJy0zQ" -Description="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" nord-cafe-pripyat-viktor-hesse-unsplash.*\
+exiftool -overwrite_original -Author="Oleksandra Bardash https://unsplash.com/@bardashka" -Comment="N/A" -Description="Wallpapers/nord-poppies-oleksandra-bardash-unsplash.png" nord-irradiated-rails-oleksandra-bardash-unsplash.*\
+exiftool -overwrite_original -Author="Oleksandra Bardash https://unsplash.com/@bardashka" -Comment="N/A" -Description="https://unsplash.com/photos/brown-wooden-house-surrounded-by-trees-under-blue-sky-during-daytime-WTMgXOH8xpQ" nord-lonely-building-oleksandra-bardash-unsplash.*\
+exiftool -overwrite_original -Author="Oleksandra Bardash https://unsplash.com/@bardashka" -Comment="N/A" -Description="https://unsplash.com/photos/red-flower-field-near-white-and-brown-house-during-daytime-W5gnyZmDyq8" nord-poppies-oleksandra-bardash-unsplash.*\
 
-exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="https://unsplash.com/photos/black-car-tires-on-brown-dirt-road-L3W-faLjBXw" -Description="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" nord-bumpers-viktor-hesse-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-corridor-mick-de-paola-unsplash.*\
 
-exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="https://unsplash.com/photos/brown-wooden-bridge-over-the-river-7mf5jP4GffQ" -Description="Chernobyl, Kyiv Oblast, Ukraine" nord-cooling-tower-base-kato-blackmore-unsplash.*\
+exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="https://unsplash.com/photos/gray-concrete-bridge-under-blue-sky-during-daytime-D_-KaJFN7WE" -Description="Chernobyl, Kyiv Oblast, Ukraine" nord-tower-pilings-kato-blackmore-unsplash.*\
 
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-corridor-mick-de-paola-unsplash.*\
+exiftool -overwrite_original -Author="Viktor Hesse Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/brown-wooden-dock-near-body-of-water-during-daytime-HVPUNrJAe18" nord-cnpp-viktor-hesse-unsplash.*\
+exiftool -overwrite_original -Author="Viktor Hesse Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/brown-and-white-concrete-house-surrounded-by-trees-during-daytime-nTZ_tFYM0WI" nord-store-viktor-hesse-unsplash.*\
 
-exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="https://unsplash.com/photos/xISVP2Q2tCQ" -Description="Chernobyl, Kyiv Oblast, Ukraine" nord-cranes-kato-blackmore-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-duga-artem-zhukov-unsplash.*\
 
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-duga-artem-zhukov-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-flowers-filip-kvasnak-unsplash.*\
 
-exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/brown-wooden-bridge-over-body-of-water-during-daytime-EBBOXbHs_hw" nord-duga-base-kato-blackmore-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-kichi-balyk-vladislav-zakharevich-unsplash.*\
 
-exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/white-and-brown-ferris-wheel-m_m5pHOsFiI" nord-duga-kato-blackmore-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-mossy-forest-douglas-schneiders-unsplash.*\
 
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-flowers-filip-kvasnak-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-orange-flowers-jessica-christian-unsplash.*\
 
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-frosty-yellow-flowers-annie-spratt-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-pripyat-pool-peter-lam-ch-unsplash.*\
 
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-irradiated-rails-oleksandra-bardash-unsplash.*\
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-stalker-vibes-tobias-reich-unsplash.*\
 
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-kichi-balyk-vladislav-zakharevich-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-mossy-forest-douglas-schneiders-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-orange-flowers-jessica-christian-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-orange-wildflowers-annie-spratt-unsplash.*\
-
-exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/brown-concrete-building-during-daytime-HVtyNCQyuew" nord-pripyat-kato-blackmore-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-pripyat-pool-peter-lam-ch-unsplash.*\
-
-exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/r0h8YSpy9tk" nord-sinking-kato-blackmore-unsplash.*\
-
-exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="Near to Pripyat city, Chernobyl's Exclusion Zone / Ukraine - October, 2019" -Description="https://unsplash.com/photos/brown-wooden-house-with-green-and-yellow-trees-bqjTzoHI-NY" nord-stadium-viktor-hesse-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-stalker-vibes-tobias-reich-unsplash.*\
-
-exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/blue-and-brown-wooden-framed-glass-window-w82zcQAH1Yg" nord-cafe-pripyat-glass-viktor-hesse-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-ukraine-road-mads-eneqvist-unsplash.*\
-
-exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="Chernobyl, Kyiv Oblast, Ukraine" -Description="https://unsplash.com/photos/brown-concrete-building-under-blue-sky-during-daytime-EU3vEzHHBJY" nord-zone-buildings-kato-blackmore-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-zone-lonely-little-building-oleksandra-bardash-unsplash.*\
-
-exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/brown-wooden-framed-glass-window-7e9sbPuvRk0" nord-pripyat-interior-viktor-hesse-unsplash.*\
-
-#exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-zone-poppies-oleksandra-bardash-unsplash.*\
-
-exiftool -overwrite_original -Author="Viktor Hesse https://unsplash.com/@vikhesse" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/green-trees-under-blue-sky-during-daytime-_04V_gEPIZU" nord-railroad-viktor-hesse-unsplash.*
-
+# exiftool -overwrite_original -Author="" -Comment="" -Description="" nord-ukraine-road-mads-eneqvist-unsplash.*
