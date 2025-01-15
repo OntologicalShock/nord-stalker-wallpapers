f50ffffba4fbc0b4716c572a668e8d1c29d10e09
OntologicalShock <justineldridgedavis@gmail.com>
2025-01-15 12:01:11 -0500

    Finished adding EXIF attribution data to each image. I may adjust the readme grid once more, but this should be the final version of the initial release.

---
+++ Wallpapers/.exif-nord.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+exiftool -overwrite_original -Author="Annie Spratt https://unsplash.com/@anniespratt" -Comment="N/A" -Description="https://unsplash.com/photos/a-close-up-of-a-plant-with-yellow-flowers-FOvTfxADPQQ" nord-frosty-yellow-flowers-annie-spratt-unsplash.*\
+
+exiftool -overwrite_original -Author="Oleksandra Bardash https://unsplash.com/@bardashka" -Comment="N/A" -Description="Wallpapers/nord-poppies-oleksandra-bardash-unsplash.png" nord-irradiated-rails-oleksandra-bardash-unsplash.*\
+
+exiftool -overwrite_original -Author="Oleksandra Bardash https://unsplash.com/@bardashka" -Comment="N/A" -Description="https://unsplash.com/photos/brown-wooden-house-surrounded-by-trees-under-blue-sky-during-daytime-WTMgXOH8xpQ" nord-lonely-building-oleksandra-bardash-unsplash.*\
+
+exiftool -overwrite_original -Author="Oleksandra Bardash https://unsplash.com/@bardashka" -Comment="N/A" -Description="https://unsplash.com/photos/red-flower-field-near-white-and-brown-house-during-daytime-W5gnyZmDyq8" nord-poppies-oleksandra-bardash-unsplash.*\
+
+exiftool -overwrite_original -Author="Nick De Paola https://unsplash.com/@mickdepaola" -Comment="https://unsplash.com/photos/grayscale-photo-of-abandoned-building-Be1L0bPhGjs" -Description="Duga Radar Facility, Chernobyl 2 - Chernobyl Exclusion Zone, Ukraine" nord-duga-corridor-mick-de-paola-unsplash.*\
+
+exiftool -overwrite_original -Author="Kato Blackmore https://unsplash.com/@katoblackmore" -Comment="https://unsplash.com/photos/gray-concrete-bridge-under-blue-sky-during-daytime-D_-KaJFN7WE" -Description="Chernobyl, Kyiv Oblast, Ukraine" nord-tower-pilings-kato-blackmore-unsplash.*\
+
+exiftool -overwrite_original -Author="Viktor Hesse Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/brown-wooden-dock-near-body-of-water-during-daytime-HVPUNrJAe18" nord-cnpp-viktor-hesse-unsplash.*\
+
+exiftool -overwrite_original -Author="Viktor Hesse Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Comment="Pripyat city - Chernobyl's Exclusion Zone / Ukraine - October, 2020. The day trip from the Chernobyl city to Prypiat" -Description="https://unsplash.com/photos/brown-and-white-concrete-house-surrounded-by-trees-during-daytime-nTZ_tFYM0WI" nord-store-viktor-hesse-unsplash.*\
+
+exiftool -overwrite_original -Author="Artem Zhukov https://unsplash.com/@artemioz" -Comment="https://unsplash.com/photos/gray-metal-fence-near-body-of-water-during-daytime-tqDElrJFTn4" -Description="Chornobyl, Київська область, Україна" nord-duga-artem-zhukov-unsplash.*\
+
+exiftool -overwrite_original -Author="Filip Kvasnak https://unsplash.com/@filipkvasnak" -Comment="https://unsplash.com/photos/a-field-full-of-red-flowers-with-the-sun-in-the-background-bIlAtcPrcAk" -Description="Slovensko, Slovensko" nord-flowers-filip-kvasnak-unsplash.*\
+
+exiftool -overwrite_original -Author="Peter Lam CH https://unsplash.com/@peterlamch" -Comment="https://unsplash.com/photos/white-stairs-inside-building-9cg6kHv4d8Q" -Description="Chernobyl, Kyiv Oblast, Ukraine" nord-pripyat-pool-peter-lam-ch-unsplash.*\
+
+exiftool -overwrite_original -Author="Tobias Reich https://unsplash.com/@electerious" -Comment="https://unsplash.com/photos/a-room-with-a-desk-and-two-windows-dF4E6AGHLYY" -Description="Chernobyl, Kyiv Oblast, Ukraine" nord-stalker-vibes-tobias-reich-unsplash.*\
+
+exiftool -overwrite_original -Author="Mads Eneqvist https://unsplash.com/@madseneqvist" -Comment="https://unsplash.com/photos/a-long-empty-road-in-the-middle-of-nowhere-ivfooFu-zCE" -Description="Chernobyl - Pripyat, Ukraine - April 2009" nord-ukraine-road-mads-eneqvist-unsplash.*
